5acd1ebd06e173ff96d81cfec6a423834662f6e8
gurenyun <18303016415@163.com>
2017-11-02 16:24:06 +0800

    'yuanduantuisong'

---
+++ webpack-app/src/components/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对React的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <add :addItem="addItem"/>
+      <list :comments="comments" :deleteItem="deleteItem" />
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import list from './list.vue';
+  import add from  './add.vue';
+  export default{
+    components:{list,add},
+    data(){
+     return {
+       comments : [
+         {username: 'eief', content: '就那样'},
+         {username: '阿斯蒂芬', content: '还可以'},
+         {username: '房东是杀手', content: '什么呀'},
+       ]
+
+     }
+    },
+    methods:{
+      addItem(comment){
+        this.comments.unshift(comment);
+      },
+      deleteItem(index){
+        this.comments.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ webpack-app/src/components/item.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="list-group-item">
+    <div class="handle">
+      <a href="javascript:;"  @click="remove">删除</a>
+    </div>
+    <p class="user"><span >{{comment.username}}</span><span>说:</span></p>
+    <p class="centence">{{comment.content}}</p>
+  </li>
+</template>
+
+<script>
+  export default{
+    props:{
+      comment:Object,
+      index:Number,
+      deleteItem:Function
+  },
+  methods:{
+      remove(){
+        const index=this.index;
+        if(confirm('确定要删除此项吗？')===true){
+          this.deleteItem(this.index);
+        }
+      }
+  }
+  }
+</script>
+
+<style>
+
+</style>
